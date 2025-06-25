4def520d74b70411bf888f1cea4a0e7cc8be7c4c
rafe_lito <rafesp0506@gmail.com>
2025-06-25 11:33:45 -0400

    feat: inicializar repositorio con estructura básica

---
+++ src/persistencia/GestorCitas.java
@@ -0,0 +1,13 @@
+package persistencia;
+
+import modelo.Cita;
+import java.io.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+public class GestorCitas {
+
+}
---
+++ src/vista/SistemaCitasHospital.java
@@ -0,0 +1,15 @@
+package vista;
+
+import modelo.Cita;
+import persistencia.GestorCitas;
+import javax.swing.*;
+import java.awt.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.List;
+
+public class SistemaCitasHospital {
+
+}
